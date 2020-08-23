4ad12e2bc6c5809a5798dd09595860246a0732be
Julian Krzefski <krzefski@heptacom.de>
2020-08-23 16:03:49 +0200

    Add argument "working-dir" to sdk:init command

--- src/Console/Command/Init.php
+++ src/Console/Command/Init.php
@@ -3,6 +3,7 @@
 namespace Heptacom\HeptaConnect\Sdk\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -23,59 +24,71 @@ class Init extends Command
 
     protected static $defaultName = 'sdk:init';
 
-    private string $vendorDir;
-
-    public function __construct(string $vendorDir)
+    protected function configure()
     {
-        parent::__construct();
-        $this->vendorDir = $vendorDir;
+        $this->addArgument('working-dir', InputArgument::OPTIONAL, 'If specified, use the given directory as working directory.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
 
-        $composerJsonPath = realpath($this->vendorDir . '/../composer.json');
-
-        if ($composerJsonPath === false) {
-            $io->error('The composer.json file of your package was not found.');
+        if (($workingDir = $this->getWorkingDir($input)) === null) {
+            $io->error(\sprintf('Invalid working directory specified, %s does not exist and is not writable.', $this->getWorkingDir($input)));
 
             return 1;
         }
 
-        $composerJson = \json_decode(\file_get_contents($composerJsonPath), true);
+        $composerJsonPath = $workingDir . '/composer.json';
+        $composerJson = \file_exists($composerJsonPath) ? \json_decode(\file_get_contents($composerJsonPath), true) : [];
+
         $packageType = $this->getPackageType($io, $composerJson);
+        $this->getPackageName($io, $workingDir, $composerJson);
+        $namespace = $this->getNamespace($io, $workingDir, $composerJson);
 
         switch ($packageType) {
             case self::KEYWORD_DATASET:
-                $this->getPackageName($io, 'heptaconnect-new-dataset', $composerJson);
-                $this->getNamespace($io, $composerJson);
                 $this->addDependency('heptacom/heptaconnect-dataset-base', '@dev', $composerJson);
 
                 break;
             case self::KEYWORD_PORTAL:
-                $this->getPackageName($io, 'heptaconnect-new-portal', $composerJson);
-                $namespace = $this->getNamespace($io, $composerJson);
                 $this->addDependency('heptacom/heptaconnect-portal-base', '@dev', $composerJson);
-
-                $this->makePortal($namespace, $io, $composerJson);
+                $this->makePortal($io, $workingDir, $namespace, $composerJson);
 
                 break;
             case self::KEYWORD_STORAGE:
-                $this->getPackageName($io, 'heptaconnect-new-storage', $composerJson);
-                $this->getNamespace($io, $composerJson);
                 $this->addDependency('heptacom/heptaconnect-storage-base', '@dev', $composerJson);
 
                 break;
             default:
-                $io->error(sprintf('Your package type is not supported by the SDK. (%s)', $packageType));
+                $io->error(\sprintf('Your package type is not supported by the SDK. (%s)', $packageType));
 
                 return 1;
         }
 
-        file_put_contents($composerJsonPath, \json_encode($composerJson, JSON_PRETTY_PRINT) . PHP_EOL);
+        \file_put_contents($composerJsonPath, \json_encode($composerJson, JSON_PRETTY_PRINT) . PHP_EOL);
 
         return 0;
+    }
+
+    private function getWorkingDir(InputInterface $input): ?string
+    {
+        if (!$input->getArgument('working-dir')) {
+            return \getcwd();
+        }
+
+        $workingDir = \realpath($input->getArgument('working-dir'));
+
+        if ($workingDir) {
+            return $workingDir;
+        }
+
+        if (\mkdir($input->getArgument('working-dir'), 0775, true) &&
+            ($workingDir = \realpath($input->getArgument('working-dir')))) {
+            return $workingDir;
+        }
+
+        return null;
     }
 
     protected function getPackageType(SymfonyStyle $io, array &$composerJson): string
@@ -85,7 +98,7 @@ class Init extends Command
         }
 
         foreach ($composerJson['keywords'] as $keyword) {
-            if (in_array($keyword, self::VALID_KEYWORDS, true)) {
+            if (\in_array($keyword, self::VALID_KEYWORDS, true)) {
                 return $keyword;
             }
         }
@@ -103,49 +116,45 @@ class Init extends Command
         $composerJson['require'][$packageName] = $version;
     }
 
-    protected function getPackageName(SymfonyStyle $io, string $nameSuggestion, array &$composerJson): string
+    protected function getPackageName(SymfonyStyle $io, string $workingDir, array &$composerJson): string
     {
         if (isset($composerJson['name']) && $composerJson['name'] !== 'heptacom/heptaconnect-skeleton') {
             return $composerJson['name'];
         }
 
-        if ($rootDir = realpath($this->vendorDir . '/../')) {
-            $nameSuggestion = basename($rootDir);
-        }
-
-        $nameSuggestion = get_current_user() . '/' . $nameSuggestion;
+        $nameSuggestion = \get_current_user() . '/' . \basename($workingDir);
 
         return $composerJson['name'] = (string) $io->ask('Please give your package a name.', $nameSuggestion);
     }
 
-    protected function getNamespace(SymfonyStyle $io, array &$composerJson): string
+    protected function getNamespace(SymfonyStyle $io, string $workingDir, array &$composerJson): string
     {
         if (isset($composerJson['autoload']['psr-4']) && !empty($composerJson['autoload']['psr-4'])) {
-            return (string) key($composerJson['autoload']['psr-4']);
+            return (string) \key($composerJson['autoload']['psr-4']);
         }
 
-        $suggestion = join('\\', array_map(function (string $name): string {
-            $name = str_replace(['-', '_'], ' ', $name);
-            $name = ucwords($name);
+        $suggestion = \join('\\', \array_map(function (string $name): string {
+            $name = \str_replace(['-', '_'], ' ', $name);
+            $name = \ucwords($name);
 
-            return str_replace(' ', '', $name);
-        }, explode('/', $composerJson['name'])));
+            return \str_replace(' ', '', $name);
+        }, \explode('/', $composerJson['name'])));
 
         $namespace = (string) $io->ask('Please specify a PSR-4 compliant namespace.', $suggestion);
-        $namespace = trim($namespace, '\\') . '\\';
+        $namespace = \trim($namespace, '\\') . '\\';
 
         $composerJson['autoload']['psr-4'][$namespace] = 'src/';
 
-        $sourceDir = $this->vendorDir . '/../' . $composerJson['autoload']['psr-4'][$namespace];
+        $sourceDir = $workingDir . '/' . $composerJson['autoload']['psr-4'][$namespace];
 
-        if (!is_dir($sourceDir)) {
-            mkdir($sourceDir, 0775, true);
+        if (!\is_dir($sourceDir)) {
+            \mkdir($sourceDir, 0775, true);
         }
 
         return $namespace;
     }
 
-    protected function makePortal(string $namespace, SymfonyStyle $io, array &$composerJson): void
+    protected function makePortal(SymfonyStyle $io, string $workingDir, string $namespace, array &$composerJson): void
     {
         if (!isset($composerJson['autoload']['psr-4'][$namespace])) {
             $io->warning('A portal could not be created, because your composer.json is missing a PSR-4 compliant autoload definition.');
@@ -153,16 +162,16 @@ class Init extends Command
             return;
         }
 
-        $sourceDir = $this->vendorDir . '/../' . $composerJson['autoload']['psr-4'][$namespace];
+        $sourceDir = $workingDir . '/' . $composerJson['autoload']['psr-4'][$namespace];
 
-        if (!is_dir($sourceDir) && !mkdir($sourceDir, 0775, true)) {
-            $io->warning(sprintf('A portal could not be created, because the source directory is not writable: %s', $sourceDir));
+        if (!\is_dir($sourceDir) && !\mkdir($sourceDir, 0775, true)) {
+            $io->warning(\sprintf('A portal could not be created, because the source directory is not writable: %s', $sourceDir));
 
             return;
         }
 
         $fileLocation = $sourceDir . '/Portal.php';
-        $namespace = rtrim($namespace, '\\');
+        $namespace = \rtrim($namespace, '\\');
         $fqn = $namespace . '\\Portal';
 
         $template = <<<"PHP"
@@ -178,7 +187,7 @@ class Portal extends PortalContract
 
 PHP;
 
-        file_put_contents($fileLocation, $template);
+        \file_put_contents($fileLocation, $template);
         $composerJson['extra']['heptaconnect']['portals'][] = $fqn;
     }
 }
